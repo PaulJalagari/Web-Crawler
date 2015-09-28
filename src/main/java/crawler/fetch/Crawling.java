87875bdde3f61b3a5ec4f0e3f9b2cb83039ba347
PaulJalagari <paul.jalagari@imaginea.com>
2015-09-28 11:47:49 +0530

    Changes

--- src/main/java/crawler/fetch/Crawling.java
+++ src/main/java/crawler/fetch/Crawling.java
@@ -16,7 +16,7 @@ import crawler.control.Spider;
 import crawler.webcrawler.NextCrawl;
 
 
-public class SpiderLeg {
+public class Crawling {
 	
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
 	private List<String> links = new LinkedList<String>();
