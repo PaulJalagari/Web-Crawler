a0fadde5a01035af721a3e5710eeb556bf9bf768
PaulJalagari <paul.jalagari@imaginea.com>
2015-09-29 11:43:26 +0530

    Changes

--- src/main/java/crawler/webcrawler/StartCrawl.java
+++ src/main/java/crawler/webcrawler/StartCrawl.java
@@ -4,13 +4,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
 
 import crawler.control.*;
 
 public class StartCrawl {
+	static Logger log = Logger.getLogger(StartCrawl.class);
 	public static void main(String args[]) throws IOException{
-		BasicConfigurator.configure();
+		PropertyConfigurator.configure("log4j.properties");
 		CrawlControl spider = new CrawlControl();
 		Properties prop = new Properties();
 		InputStream input = null;
